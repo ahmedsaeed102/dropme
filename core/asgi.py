06ed816c66c4ccb7781cc634d9bc2d319408aff3
Khaled Emad <khaledemad656@gmail.com>
2023-03-15 17:26:26 +0200

    fix number 6

--- core/asgi.py
+++ core/asgi.py
@@ -1,10 +1,11 @@
 import os
 import django
-django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
 django_asgi_app  = get_asgi_application()
 
 import machine_api.routing
