4d6c99ff6958cd0b831cbec8c53ad1170b3635f5
Khaled Emad <khaledemad656@gmail.com>
2023-03-15 16:48:24 +0200

    fix daphine issue

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,14 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-# from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-from .channelsmiddleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-django_asgi_app  = get_asgi_application()
-
 import machine_api.routing
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from .channelsmiddleware import TokenAuthMiddleware
+
+django_asgi_app  = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
