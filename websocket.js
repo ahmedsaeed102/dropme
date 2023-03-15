35dcb8fb9052b4fb4cf318ad8741f3783273a2be
Khaled Emad <khaledemad656@gmail.com>
2023-03-15 18:39:33 +0200

    fixed error hopefully

--- websocket.js
+++ websocket.js
@@ -1,5 +1,5 @@
 let token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgxMzk2MzUxLCJpYXQiOjE2Nzg4MDQzNTEsImp0aSI6ImZiNTc4NzVkYzlmNzQ1Y2E4MjBkMmQ3ODAzYWRiODBhIiwidXNlcl9pZCI6MTR9.9niez_A8isrxCgX7xibZPyT2xFrNSpI90Xp_lj2tfFI'
-// let endpoint = "ws://localhost:8000/machines/recycle/start/machine/"
+// let endpoint = "wss://localhost:443/machines/recycle/start/machine/"
 let endpoint = "wss://dropme.up.railway.app/machines/recycle/start/machine/"
 const ws = new WebSocket(endpoint + "?token=" + token)
 
