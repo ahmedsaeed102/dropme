e24ffd98f56f324f301f419e5d21630dfbdb6336
Khaled Emad <khaledemad656@gmail.com>
2023-03-15 15:50:29 +0200

    edit trusted origin

--- websocket.js
+++ websocket.js
@@ -1,5 +1,6 @@
 let token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgxMzk2MzUxLCJpYXQiOjE2Nzg4MDQzNTEsImp0aSI6ImZiNTc4NzVkYzlmNzQ1Y2E4MjBkMmQ3ODAzYWRiODBhIiwidXNlcl9pZCI6MTR9.9niez_A8isrxCgX7xibZPyT2xFrNSpI90Xp_lj2tfFI'
-let endpoint = "ws://localhost:8000/machines/recycle/start/machine/"
+// let endpoint = "ws://localhost:8000/machines/recycle/start/machine/"
+let endpoint = "ws://web-production-86d4.up.railway.app/machines/recycle/start/machine/"
 const ws = new WebSocket(endpoint + "?token=" + token)
 
 ws.addEventListener('message', (event) => {
