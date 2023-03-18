b5aa03204dc961c9b0225f32f12c3105a4225532
Khaled Emad <khaledemad656@gmail.com>
2023-03-18 18:09:54 +0200

    edit machine model

--- machine_api/models.py
+++ machine_api/models.py
@@ -27,7 +27,6 @@ class Machine(models.Model):
         decimal_places = 6,
         blank= True,
         null=True,
-        help_text = _('Latitdue, Format:required')
     )
 
     city = models.CharField(max_length=50, null=True, help_text = _('Machine city'))
