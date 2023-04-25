999be139a406a921061b6a4d14c75ac44ff11e96
Khaled Emad <khaledemad656@gmail.com>
2023-04-25 15:43:40 +0200

    test

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,7 @@ cert = {
     "type": os.environ.get('type'), 
     "project_id": os.environ.get('project_id'),
     "private_key_id": os.environ.get('private_key_id'),
-    "private_key": f"{os.environ.get('private_key')}".replace(r'\n', '\n'),
+    "private_key": os.environ.get('private_key').replace(r'\n', '\n'),
     "client_email": os.environ.get('client_email'),
     "client_id": os.environ.get('client_id'),
     "auth_uri": os.environ.get('auth_uri'),
