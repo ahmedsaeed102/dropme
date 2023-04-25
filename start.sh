7a0d756effb17ec5ba080d8cfd25269b91e194ec
Khaled Emad <khaledemad656@gmail.com>
2023-04-25 14:57:29 +0200

    test

--- start.sh
+++ start.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-cat > .env << EOL
-EMAIL_HOST_USER=${EMAIL_HOST_USER}
-EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}
-state=${state}
-SITE_ID=${SITE_ID}
-apikey=${apikey}
-db_password=${db_password}
-db_host=${db_host}
-db_port=${db_port}
-db_name=${db_name}
-type=${type}
-project_id=${project_id}
-private_key_id=${private_key_id}
-private_key=${private_key}
-SECRET_KEY="${SECRET_KEY}"
-client_email=${client_email}
-client_id=${client_id}
-auth_uri=${auth_uri}
-token_uri=${token_uri}
-auth_provider_x509_cert_url=${auth_provider_x509_cert_url}
-client_x509_cert_url=${client_x509_cert_url}
-AWS_S3_REGION_NAME=${AWS_S3_REGION_NAME}
-AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-AWS_STORAGE_BUCKET_NAME=${AWS_STORAGE_BUCKET_NAME}
-EOL
+# cat > .env << EOL
+# EMAIL_HOST_USER=${EMAIL_HOST_USER}
+# EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}
+# state=${state}
+# SITE_ID=${SITE_ID}
+# apikey=${apikey}
+# db_password=${db_password}
+# db_host=${db_host}
+# db_port=${db_port}
+# db_name=${db_name}
+# type=${type}
+# project_id=${project_id}
+# private_key_id=${private_key_id}
+# private_key=${private_key}
+# SECRET_KEY="${SECRET_KEY}"
+# client_email=${client_email}
+# client_id=${client_id}
+# auth_uri=${auth_uri}
+# token_uri=${token_uri}
+# auth_provider_x509_cert_url=${auth_provider_x509_cert_url}
+# client_x509_cert_url=${client_x509_cert_url}
+# AWS_S3_REGION_NAME=${AWS_S3_REGION_NAME}
+# AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+# AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+# AWS_STORAGE_BUCKET_NAME=${AWS_STORAGE_BUCKET_NAME}
+# EOL
 
 python3 manage.py makemigrations 
 python3 manage.py migrate
