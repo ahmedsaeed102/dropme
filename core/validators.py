81b7a62c62a35ad5733bd819fd0ac4ffb085ba67
Sohila Ahmed <sohila.ahmede@uniparticle.com>
2024-10-19 20:30:03 +0300

    fix mobile number in registeration

--- core/validators.py
+++ core/validators.py
@@ -7,5 +7,5 @@ def validate_phone_number(value):
     if not re.match(r"^1[0125]{1}", value):
         raise ValidationError(_("Phone number must start with 010 or 011 or 012 or 015"))
 
-    if not re.match(r"^\d{11}$", value):
+    if not re.match(r"^\d{10}$", value):
         raise ValidationError(_("Phone number must be 11 numbers"))
