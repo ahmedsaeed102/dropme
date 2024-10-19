8ba7bd06c5ff155f0653df29a12d0cc5c5a2277c
Sohila Ahmed <sohila.ahmede@uniparticle.com>
2024-10-19 20:20:15 +0300

    Refactoring user AOIs + prefered languages APIs

--- users_api/admin.py
+++ users_api/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from .models import UserModel, LocationModel, Feedback
 
-
 @admin.register(UserModel)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "email",
-        "phone_number",
-        "total_points",
-        "address",
-    )
-
+    list_display = ("id", "email", "username", "phone_number", "total_points", "address", "registered_at")
+    search_fields = ("email", "phone_number", "username")
+    list_filter = ("is_staff",)
+    ordering = ("registered_at",)
 
 admin.site.register(LocationModel)
 admin.site.register(Feedback)
--- users_api/models.py
+++ users_api/models.py
@@ -7,6 +7,9 @@ from model_utils import Choices
 from core.validators import validate_phone_number
 from .managers import UserManager
 
+class LanguageChoices(models.TextChoices):
+    ARABIC = 'ARABIC', 'Arabic'
+    ENGLISH = 'ENGLISH', 'English'
 
 def upload_to(instance, filename):
     return f"upload_to/{instance.username}/{filename}"
@@ -18,24 +21,10 @@ class LocationModel(models.Model):
     def __str__(self):
         return self.address
 
-
 class UserModel(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50)
-    email = models.EmailField(
-        unique=True,
-        null=False,
-        blank=False,
-        max_length=50,
-        validators=[validate_email],
-        verbose_name=_("Email"),
-    )
-    phone_number = models.CharField(
-        unique=True,
-        null=True,
-        blank=True,
-        max_length=11,
-        validators=[validate_phone_number],
-    )
+    email = models.EmailField(unique=True, null=False, blank=False, max_length=50, validators=[validate_email], verbose_name=_("Email"))
+    phone_number = models.CharField(unique=True, null=True, blank=True, max_length=11, validators=[validate_phone_number])
 
     otp = models.CharField(max_length=4)
     otp_expiration = models.DateTimeField(null=True, blank=True)
@@ -46,27 +35,15 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     registered_at = models.DateTimeField(auto_now_add=True)
 
-    profile_photo = models.ImageField(
-        upload_to=upload_to, default="upload_to/default.png"
-    )
-
+    profile_photo = models.ImageField(upload_to=upload_to, default="upload_to/default.png")
     age = models.IntegerField(null=True, blank=True)
-
     GENDER = Choices("male", "female")
     gender = models.CharField(choices=GENDER, default=GENDER.male, max_length=20)
-
     total_points = models.PositiveIntegerField(default=0)
-
-    address = models.ForeignKey(
-        LocationModel,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="address_name",
-    )
+    address = models.ForeignKey(LocationModel, on_delete=models.CASCADE, null=True, blank=True, related_name="address_name")
+    prefered_language = models.CharField(max_length=2, choices=LanguageChoices.choices, default=LanguageChoices.ENGLISH)
 
     objects = UserManager()
-
     USERNAME_FIELD = "email"
 
     def __str__(self):
@@ -85,9 +62,7 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
 class Feedback(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
-    user = models.ForeignKey(
-        UserModel, on_delete=models.CASCADE, related_name="feedbacks"
-    )
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="feedbacks")
 
     def __str__(self):
         return self.title
